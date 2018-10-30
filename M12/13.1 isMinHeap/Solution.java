442b90d1340f35ab9139e4aa13e6b13879924293
shiva sai yadav <shivasaiyadav1@gmail.com>
2018-10-30 14:40:43 +0530

    "check_style_score -> 4 of 5 passed. style score: 0/1 "

--- M12/13.1 isMinHeap/Solution.java
+++ M12/13.1 isMinHeap/Solution.java
@@ -1,17 +1,12 @@
-
-
 import java.util.Scanner;
-
-public class Solution
-{
-	public static void main(String args[])
-	{
+public class Solution {
+	public static void main(String args[]) {
 		Scanner scn = new Scanner(System.in);
 		String ip = scn.nextLine();
 		if (ip.equals("String")) {
 			int m = scn.nextInt();
 			for (int k = 0; k < m; k++) {
-				String st = scn.nextLine();
+				String st = scn.next();
 				String sp[] = st.split(",");
 				int n = sp.length;
 				boolean flag = true;
@@ -19,42 +14,11 @@ public class Solution
 					// System.out.println("true");
 					int l = 2 * i + 1; // left = 2*i + 1
 					int r = 2 * i + 2; // right = 2*i + 2
-					if (l < n && sp[i].compareTo(sp[l]) > 0) 
-					{
+					if (l < n && sp[i].compareTo(sp[l]) > 0) {
 						flag = false;
 						break;
 					}
-					if (r < n && sp[i].compareTo(sp[r]) > 0)
-					{
-						flag = false;
-						break;
-					}
-
-				}
-
-				System.out.println(flag);
-			}
-		}
-		else if(ip.equals("Double"))
-		{
-			int m = scn.nextInt();
-			for (int k = 0; k < m; k++)
-			{
-				String st = scn.next();
-				String sp[] = st.split(",");
-				Double b[]=new Double[sp.length];
-				int n = sp.length;
-				boolean flag = true;
-				for (int i = 0; i < n / 2 - 1; i++) 
-				{
-					// System.out.println("true");
-					int l = 2 * i + 1; // left = 2*i + 1
-					int r = 2 * i + 2; // right = 2*i + 2
-					if (l < n &&Double.parseDouble((sp[i]))  > Double.parseDouble((sp[l]))) {
-						flag = false;
-						break;
-					}
-					if (r < n && Double.parseDouble(sp[i]) >Double.parseDouble(sp[r])) {
+					if (r < n && sp[i].compareTo(sp[r]) > 0) {
 						flag = false;
 						break;
 					}
@@ -63,67 +27,40 @@ public class Solution
 
 				System.out.println(flag);
 			}
-		}
-		else if(ip.equals("Integer"))
-		{
-			int m = scn.nextInt();
-			for (int k = 0; k < m; k++)
-			{
-				String st = scn.next();
-				String sp[] = st.split(",");
-				Integer b[]=new Integer[sp.length];
-				int n = sp.length;
-				boolean flag = true;
-				for (int i = 0; i < n / 2 - 1; i++) 
-				{
-					// System.out.println("true");
-					int l = 2 * i + 1; // left = 2*i + 1
-					int r = 2 * i + 2; // right = 2*i + 2
-					if (l < n &&Integer.parseInt((sp[i]))  > Integer.parseInt((sp[l]))) {
-						flag = false;
-						break;
-					}
-					if (r < n && Integer.parseInt(sp[i]) > Integer.parseInt(sp[r])) {
-						flag = false;
-						break;
-					}
-				}
-				System.out.println(flag);
-			}
-		}
-		else if(ip.equals("Float"))
-		{
+		} else {
 			int m = scn.nextInt();
 			int count=0;
-			for (int k = 0; k < m; k++)
-			{
+			for (int k = 0; k < m; k++) {
 				String st = scn.next();
 				String sp[] = st.split(",");
-				Float b[]=new Float[sp.length];
-				int n = sp.length;
-				boolean flag = true;
-				for (int i = 0; i < n / 2 - 1; i++) 
-				{
-					// System.out.println("true");
-					int l = 2 * i + 1; // left = 2*i + 1
-					int r = 2 * i + 2; // right = 2*i + 2
-					if (l < n &&Float.parseFloat((sp[i]))  > Float.parseFloat((sp[l]))) {
-						flag = false;
-						break;
+				Double b[] = new Double[sp.length];
+				if (b.length == 0) {
+//					System.out.println("false");
+				} else {
+					int n = sp.length;
+					boolean flag = true;
+					for (int i = 0; i < n / 2 - 1; i++) {
+						// System.out.println("true");
+						int l = 2 * i + 1; // left = 2*i + 1
+						int r = 2 * i + 2; // right = 2*i + 2
+						if (l < n && Double.parseDouble((sp[i])) > Double.parseDouble((sp[l]))) {
+							flag = false;
+							break;
+						}
+						if (r < n && Double.parseDouble(sp[i]) > Double.parseDouble(sp[r])) {
+							flag = false;
+							break;
+						}
+
 					}
-					if (r < n && Float.parseFloat(sp[i]) > Float.parseFloat(sp[r])) {
-						flag = false;
-						break;
-					}
+
+					System.out.println(flag);
 				}
-				System.out.println(flag);
 				count++;
 			}
-			if(count<m)
-			{
-				System.out.println(false);
+			if(count<m) {
+				System.out.println("false");
 			}
 		}
 	}
 }
-
