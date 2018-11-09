a0c1e8377043fe39becea719f08d767ade4b5f10
shiva sai yadav <shivasaiyadav1@gmail.com>
2018-11-09 12:56:40 +0530

    "check_style_score -> 3 of 6 passed. style score: 0/1 "

--- M21/22.1 Ransom Note/Solution.java
+++ M21/22.1 Ransom Note/Solution.java
@@ -1,27 +1,31 @@
 
 import java.util.Scanner;
 import java.util.HashMap;
+import java.util.Map;
 public class Solution
-{
-	
+{	
 public static void main(String args[])
 {
 	
-	Scanner sc= new Scanner(System.in);
-	int a= sc.nextInt();
-	int b= sc.nextInt();
-	String s1 = sc.nextLine();
-	String s2 = sc.nextLine();
-	HashMap<String, String> hm = new HashMap< String, String>();
-	hm.put(s1, s2);
-	int i=0;
-	if(hm.containsValue(s2))
+	Scanner sc=new Scanner(System.in);
+    String s1[]=sc.nextLine().split(" ");
+    int a = Integer.parseInt(s1[0]);
+    int b = Integer.parseInt(s1[1]);
+    String s2 = sc.nextLine();
+    String s3 = sc.nextLine();
+	HashMap<String, Integer > hm = new HashMap<>();
+	HashMap<String, Integer> mag = new HashMap<>();
+	hm.put(s2, a);
+	mag.put(s3, b);
+	if(hm.keySet().contains(mag.keySet()))
 	{
-		System.out.println("Yes");
+	System.out.println("No");
 	}
 	else
 	{
-		System.out.println("no");
+		System.out.println("Yes");
 	}
+		
+	
 }
 }
