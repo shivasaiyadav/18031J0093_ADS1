73ea86d2367055629c41cf894d3e877c11834bd3
shiva sai yadav <shivasaiyadav1@gmail.com>
2018-11-09 15:18:38 +0530

    "check_style_score -> 5 of 6 passed. style score: 0/1 "

--- M21/22.1 Ransom Note/Solution.java
+++ M21/22.1 Ransom Note/Solution.java
@@ -1,31 +1,63 @@
-
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-public class Solution
-{	
-public static void main(String args[])
-{
-	
-	Scanner sc=new Scanner(System.in);
-    String s1[]=sc.nextLine().split(" ");
-    int a = Integer.parseInt(s1[0]);
-    int b = Integer.parseInt(s1[1]);
-    String s2 = sc.nextLine();
-    String s3 = sc.nextLine();
-	HashMap<String, Integer > hm = new HashMap<>();
-	HashMap<String, Integer> mag = new HashMap<>();
-	hm.put(s2, a);
-	mag.put(s3, b);
-	if(hm.keySet().contains(mag.keySet()))
-	{
-	System.out.println("No");
-	}
-	else
-	{
-		System.out.println("Yes");
-	}
-		
-	
-}
+import java.util.Scanner;
+
+public class Solution {
+  Map<String, Integer> magazineMap;
+  Map<String, Integer> noteMap;
+
+  public Solution(String magazine, String note) { 
+    Collection<String> magazineStrings = Arrays.asList(magazine.split(" "));
+    Collection<String> noteStrings = Arrays.asList(note.split(" "));
+    magazineMap = new HashMap<String,Integer>();
+    noteMap = new HashMap<String,Integer>();
+
+   
+
+    for(String value : magazineStrings) {
+      if (magazineMap.get(value) == null) {
+        magazineMap.put(value, 0);  
+      }
+      Integer currentValue = magazineMap.get(value);
+      currentValue++;
+      magazineMap.put(value,currentValue);
+    }
+
+    for(String value : noteStrings) {
+      if (noteMap.get(value) == null) {
+        noteMap.put(value, 0);  
+      }
+      Integer currentValue = noteMap.get(value);
+      currentValue++;
+      noteMap.put(value,currentValue);
+    }
+  }
+
+  public boolean solve() {
+    for(String ransomWord : noteMap.keySet()) {
+      Integer ransomValue   = noteMap.get(ransomWord);
+      Integer magazineValue = magazineMap.get(ransomWord);
+
+      if (magazineValue == null || ransomValue > magazineValue) {
+        return false;  
+      }
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int m = scanner.nextInt();
+    int n = scanner.nextInt();
+	scanner.nextLine();
+    Solution s = new Solution(scanner.nextLine(), scanner.nextLine());
+    scanner.close();
+
+    boolean answer = s.solve();
+    if(answer)
+      System.out.println("Yes");
+    else System.out.println("No");
+  }
 }
