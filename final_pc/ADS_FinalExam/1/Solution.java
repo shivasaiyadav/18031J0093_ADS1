d21bd6a8f988d4b22851438264f274797c9a1694
shiva sai yadav <shivasaiyadav1@gmail.com>
2018-11-10 11:52:01 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ final_pc/ADS_FinalExam/1/Solution.java
@@ -0,0 +1,56 @@
+
+import java.util.Hashtable;
+import java.util.Scanner;
+
+class Student
+{
+	String name;
+	double marks;
+	public Student(String name, double marks)
+	{
+		this.name=name;
+		this.marks=marks;
+	}
+}
+public class Solution 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		Hashtable<Integer,Student> hst = new Hashtable<Integer,Student>();
+		String s1 = sc.nextLine();
+		int n = Integer.parseInt(s1);
+		int i = 0 ;
+		while(i<n)
+		{
+		String input = sc.nextLine();
+		String []d1 = input.split(",");
+			Student st = new Student(d1[1],Double.parseDouble(d1[2]));
+			hst.put(Integer.parseInt(d1[0]), st);
+			i++;
+		}
+		String s2 = sc.nextLine();
+		int m = Integer.parseInt(s2);
+		int j = 0 ;
+		while(j<m)
+		{
+			String output = sc.nextLine();
+			String []d2=output.split(" ");
+			if(!hst.containsKey(Integer.parseInt(d2[2])==1))
+			{
+				Student st1 = hst.get(Integer.parseInt(d2[1]));
+				System.out.println(st1.name);
+			}
+			else if(Integer.parseInt(d2[2])==2)
+			{
+				Student st1 = hst.get(Integer.parseInt(d2[1]));
+				System.out.println(st1.marks);
+			}
+			else if(!hst.containsKey(Integer.parseInt(d2[1])))
+			{
+				System.out.println("Student doesn't exists...");
+			}	
+			i++;
+		}
+	}
+}
